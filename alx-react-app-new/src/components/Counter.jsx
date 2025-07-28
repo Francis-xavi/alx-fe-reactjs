9f36c329d9ea49426163f880136b12bb0d278ffc
Bryan Udo-Ndupu <104767742+Francis-xavi@users.noreply.github.com>
2025-07-28 15:56:58 +0100

    updated count

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 
 
 export default function Counter() {
-    const [increase, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
     return (
         <div>
             <p>you clicked {increase} </p>
-            <button onClick={() => setCount(increase + 1)}>Increment</button>
-            <button onClick={() => setCount(increase - 1)}>Decrement</button>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
         </div>
     );
