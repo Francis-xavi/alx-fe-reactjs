72a2d6a649287bbe6a442ffa95c8e770f79074ad
Bryan Udo-Ndupu <104767742+Francis-xavi@users.noreply.github.com>
2025-07-28 15:51:59 +0100

    update

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile(Props) {
     return (
-        <div style={{ padding: '10px', margin: '10px' }}>
+        <div style={{ border: '1px solid blue', padding: '10px', margin: '10px' }}>
             <h2 style={{ color: 'red'}}>{Props.name}</h2>
             <p> <span style={{ fontWeight: 'bold' }}>{Props.age}</span></p>
             <p> <span style={{ color: 'blue' }} >{Props.bio}</span></p>
