e807937a908a5f65f2cc2ee995bdcb3ea44656ee
Bryan Udo-Ndupu <104767742+Francis-xavi@users.noreply.github.com>
2025-07-29 22:54:29 +0100

    unfix

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
 import UserContext from './UserContext';
 
-function UserProfile() {
+function UserProfile(props) {
     return (
         <div>
-            <h2>{UserContext.name}</h2>
-            <p>Age: {UserContext.age}</p>
-            <p>Bio: {UserContext.bio}</p>
+            <h2>{props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
         </div>
     );
 }
